de51d669cb72eb20f3134897f23e470b9b93d974
Dila Gedik <gedikd@bzz.ch>
2022-01-04 17:32:16 +0100

    Produkte 4-6 hinzugefügt

--- js/katalog_elemente.js
+++ js/katalog_elemente.js
@@ -1,36 +1,36 @@
 //PRODUKTE
 let produkt1 = {
-    "prduktnummer": 101,
+    "produktnummer": 101,
     "produktname": "Schoko Donut Pin",
     "farbe": "Schokolade",
     "preis": "CHF 15.-",
 }
 let produkt2 = {
-    "prduktnummer": 102,
+    "produktnummer": 102,
     "produktname": "Erdbeer Donut Pin",
     "farbe": "Erdbeer",
     "preis": "CHF 15.-",
 }
 let produkt3 = {
-    "prduktnummer": 103,
+    "produktnummer": 103,
     "produktname": "Vanille Donut Pin",
     "farbe": "Vanille",
     "preis": "CHF 15.-",
 }
 let produkt4 = {
-    "prduktnummer": 104,
+    "produktnummer": 104,
     "produktname": "Schoko Donut Tote Bag",
     "farbe": "Schokolade",
     "preis": "CHF 20.-",
 }
 let produkt5 = {
-    "prduktnummer": 105,
+    "produktnummer": 105,
     "produktname": "Erdbeer Donut Tote Bag",
     "farbe": "Erdbeer",
     "preis": "CHF 20.-",
 }
 let produkt6 = {
-    "prduktnummer": 106,
+    "produktnummer": 106,
     "produktname": "Vanille Donut Tote Bag",
     "farbe": "Vanille",
     "preis": "CHF 20.-",
@@ -44,16 +44,37 @@ htmlObj.innerHTML =
     `Preis: ${produkt1.preis} <br>`+
     `Produktnummer: ${produkt1.produktnummer} <br>`;
 //PRODUKT 2 AUF PAGE
-let htmlObj = document.getElementById("produkt2");
+htmlObj = document.getElementById("produkt2");
 htmlObj.innerHTML =
     `Produktname: ${produkt2.produktname} <br>`+
     `Farbe: ${produkt2.farbe} <br>`+
     `Preis: ${produkt2.preis} <br>`+
     `Produktnummer: ${produkt2.produktnummer} <br>`;
 //PRODUKT 3 AUF PAGE
-let htmlObj = document.getElementById("produkt3");
+htmlObj = document.getElementById("produkt3");
 htmlObj.innerHTML =
     `Produktname: ${produkt3.produktname} <br>`+
     `Farbe: ${produkt3.farbe} <br>`+
     `Preis: ${produkt3.preis} <br>`+
     `Produktnummer: ${produkt3.produktnummer} <br>`;
+//PRODUKT 4 AUF PAGE
+htmlObj = document.getElementById("produkt4");
+htmlObj.innerHTML =
+    `Produktname: ${produkt4.produktname} <br>`+
+    `Farbe: ${produkt4.farbe} <br>`+
+    `Preis: ${produkt4.preis} <br>`+
+    `Produktnummer: ${produkt4.produktnummer} <br>`;
+//PRODUKT 5 AUF PAGE
+htmlObj = document.getElementById("produkt5");
+htmlObj.innerHTML =
+    `Produktname: ${produkt5.produktname} <br>`+
+    `Farbe: ${produkt5.farbe} <br>`+
+    `Preis: ${produkt5.preis} <br>`+
+    `Produktnummer: ${produkt5.produktnummer} <br>`;
+//PRODUKT 6 AUF PAGE
+htmlObj = document.getElementById("produkt6");
+htmlObj.innerHTML =
+    `Produktname: ${produkt6.produktname} <br>`+
+    `Farbe: ${produkt6.farbe} <br>`+
+    `Preis: ${produkt6.preis} <br>`+
+    `Produktnummer: ${produkt6.produktnummer} <br>`;
