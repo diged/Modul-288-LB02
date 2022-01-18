25314af38c572f7e1c87a95f168b0411b95e043b
Dila Gedik <gedikd@bzz.ch>
2022-01-18 15:41:26 +0100

    - Bilder Katalog hinzugefügt

--- js/katalog_elemente2.js
+++ js/katalog_elemente2.js
@@ -6,81 +6,81 @@ let produktListe =
             "bildid": "pi1",
             "produktnummer": 101,
             "produktname": "Donut Pin",
-            "farbe": "Schokolade",
+            "sorte": "Schokolade",
             "preis": "CHF 15.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Donut_Pin_Schoko.jpg"
         },
         {
             "pid": "produkt2",
             "bildid": "pi2",
             "produktnummer": 102,
             "produktname": "Donut Pin",
-            "farbe": "Erdbeer",
+            "sorte": "Erdbeere",
             "preis": "CHF 15.-",
-            "path": "../images/Donut_Blanc.jpg"
+            "path": "../images/Donut_Pin_Erdbeere.jpg"
         },
         {
             "pid": "produkt3",
             "bildid": "pi3",
             "produktnummer": 103,
             "produktname": "Donut Pin",
-            "farbe": "Vanille",
+            "sorte": "Vanille",
             "preis": "CHF 15.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Donut_Pin_Vanille.jpg"
         },
         {
             "pid": "produkt4",
             "bildid": "pi4",
             "produktnummer": 104,
             "produktname": "Donut Tote Bag",
-            "farbe": "Schokolade",
+            "sorte": "Schokolade",
             "preis": "CHF 20.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Tote_Bag_Schoko.jpg"
         },
         {
             "pid": "produkt5",
             "bildid": "pi5",
             "produktnummer": 105,
             "produktname": "Donut Tote Bag",
-            "farbe": "Erdbeer",
+            "sorte": "Erdbeere",
             "preis": "CHF 20.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Tote_Bag_Erdbeere.jpg"
         },
         {
             "pid": "produkt6",
             "bildid": "pi6",
             "produktnummer": 106,
             "produktname": "Donut Tote Bag",
-            "farbe": "Vanille",
+            "sorte": "Vanille",
             "preis": "CHF 20.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Tote_Bag_Vanille.jpg"
         },
         {
             "pid": "produkt7",
             "bildid": "pi7",
             "produktnummer": 107,
-            "produktname": "4er Donut-Box (gemischt)",
-            "donutSorten": "Schokolade, Erbeere, Vanille, Caramel",
+            "produktname": "4er Donut-Box",
+            "sorte": "Schokolade, Erbeere, Vanille, Caramel",
             "preis": "CHF 12.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Donut_Box_4.jpg"
         },
         {
             "pid": "produkt8",
             "bildid": "pi8",
             "produktnummer": 108,
-            "produktname": "6er Donut-Box (gemischt)",
-            "donutSorten": "Schokolade, Erbeere, Vanille, Caramel, Marshmallow, Monster",
+            "produktname": "6er Donut-Box",
+            "sorte": "Schokolade, Erbeere, Vanille, Caramel, Marshmallow, Monster",
             "preis": "CHF 16.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Donut_Box_6.jpg"
         },
         {
             "pid": "produkt9",
             "bildid": "pi9",
             "produktnummer": 109,
-            "produktname": "8er Donut-Box (gemischt)",
-            "donutSorten": "Schokolade, Erbeere, Vanille, Caramel, Marshmallow, Monster",
+            "produktname": "8er Donut-Box",
+            "sorte": "Schokolade, Erbeere, Vanille, Caramel, Marshmallow, Monster",
             "preis": "CHF 20.-",
-            "path": "../images/Donut_Pin_Schoko.png"
+            "path": "../images/Donut_Box_8.jpg"
         }
     ];
 //PRODUKTE AUF DER KATALOGSEITE
@@ -100,11 +100,10 @@ let produktListe =
 //ZWEITE VERSION:
    produktListe.forEach(item => {
         console.log(`${item.pid}`);
-        console.log(`${item.pid}`);
         let htmlObj = document.getElementById(item.pid);
         htmlObj.innerHTML =
             `<b>Produktname:</b> ${item.produktname} <br>`+
-            `<b>Farbe:</b> ${item.farbe} <br>`+
+            `<b>Sorte:</b> ${item.sorte} <br>`+
             `<b>Preis:</b> ${item.preis} <br>`+
             `<b>Produktnummer:</b> ${item.produktnummer} <br>`;
         htmlObj = document.getElementById(item.bildid);
